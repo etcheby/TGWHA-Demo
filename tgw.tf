42167e04b22125a34d18d30028da13d5952c3aac
Eugene Tcheby <68718382+etcheby@users.noreply.github.com>
2020-12-20 00:40:02 -0500

    Update tgw.tf

--- tgw.tf
+++ tgw.tf
@@ -4,12 +4,12 @@
 
 # Create the TGW
 resource "aws_ec2_transit_gateway" "transit_gateway" {
-  description = "TGW to demo Check Point Geocluster"
-  auto_accept_shared_attachments = "enable"
+  description                     = "TGW to demo Check Point Geocluster"
+  auto_accept_shared_attachments  = "enable"
   default_route_table_association = "disable"
   default_route_table_propagation = "disable"
   tags = {
-    Name        = "Demo-TGW"
+    Name = "TGW"
   }
 }
 
@@ -19,9 +19,9 @@ resource "aws_ec2_transit_gateway" "transit_gateway" {
 
 # Attach the Management VPC to the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "mgmt_attachment" {
-  subnet_ids         = ["${aws_subnet.mgmt_subnet.id}"]
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
-  vpc_id             = aws_vpc.mgmt_vpc.id
+  subnet_ids                                      = ["aws_subnet.mgmt_subnet.id"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.transit_gateway.id
+  vpc_id                                          = aws_vpc.mgmt_vpc.id
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
 
@@ -32,9 +32,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "mgmt_attachment" {
 
 # Attach Security VPC to the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "security_attachment" {
-  subnet_ids         = ["${aws_subnet.tgw_subnet1.id}","${aws_subnet.tgw_subnet2.id}"]
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
-  vpc_id             = aws_vpc.geocluster_vpc.id
+  subnet_ids                                      = ["var.tgw_subnet1", "var.tgw_subnet2"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.transit_gateway.id
+  vpc_id                                          = var.geocluster_vpc
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
 
@@ -45,9 +45,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "security_attachment" {
 
 # Attach Spoke1 VPC to the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "spoke1_vpc_attachment" {
-  subnet_ids         = ["${aws_subnet.spoke1_subnet.id}"]
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
-  vpc_id             = aws_vpc.spoke1_vpc.id
+  subnet_ids                                      = ["aws_subnet.spoke1_subnet.id"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.transit_gateway.id
+  vpc_id                                          = aws_vpc.spoke1_vpc.id
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
 
@@ -58,9 +58,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "spoke1_vpc_attachment" {
 
 # Attach Spoke2 VPC to the TGW
 resource "aws_ec2_transit_gateway_vpc_attachment" "spoke2_vpc_attachment" {
-  subnet_ids         = ["${aws_subnet.spoke2_subnet.id}"]
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
-  vpc_id             = aws_vpc.spoke2_vpc.id
+  subnet_ids                                      = ["aws_subnet.spoke2_subnet.id"]
+  transit_gateway_id                              = aws_ec2_transit_gateway.transit_gateway.id
+  vpc_id                                          = aws_vpc.spoke2_vpc.id
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
 
